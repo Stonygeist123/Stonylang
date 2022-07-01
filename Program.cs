58dfb5781edaf227503b9ba1d598319d08144374
Stonygeist123 <65509673+Stonygeist123@users.noreply.github.com>
2022-07-01 15:32:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stonylang_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
